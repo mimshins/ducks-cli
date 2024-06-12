f9fbd80326af938af1ddebd695b0d5dc125d92b8
mimshins <mostafa.sh.coderino@gmail.com>
2024-06-12 17:39:07 +0330

    [✨][app]: add new utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,3 +4,5 @@ export {
   useDeterministicId,
 } from "@dot-helix/ui/utils";
 export { default as useMediaQuery } from "@utilityjs/use-media-query";
+export { default as useScrollGuard } from "@utilityjs/use-scroll-guard";
+export * from "./is-absolute-url";
